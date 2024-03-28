12e3e17fa82946b79f2aa0c7fc95dcb7962fef98
Mary Danyliuk <marydanyliuk@gmail.com>
2024-03-28 19:34:25 +0200

    add styles

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -9,6 +9,28 @@ function toggleBurgerMenu(event) {
   refs.mobileMenu.classList.toggle(`open`);
 }
 
+// document.addEventListener('DOMContentLoaded', function () {
+//   const burgerMenu = document.querySelector('.menu-container');
+//   const closeButton = burgerMenu.querySelector('#close_burger_btn');
+//   const menuItems = burgerMenu.querySelectorAll('.mobile-menu__item a');
+
+//   closeButton.addEventListener('click', function () {
+//     burgerMenu.classList.remove('menu-active');
+//   });
+
+//   menuItems.forEach(function (item) {
+//     item.addEventListener('click', function (e) {
+//       e.preventDefault();
+//       const target = this.getAttribute('href').substring(1);
+//       const section = document.getElementById(target);
+//       if (section) {
+//         section.scrollIntoView({ behavior: 'smooth' });
+//         burgerMenu.classList.remove('menu-active');
+//       }
+//     });
+//   });
+// });
+
 // function toggleBurgerMenu(event) {
 //   event.preventDefault();
 //   const user = auth.currentUser;
