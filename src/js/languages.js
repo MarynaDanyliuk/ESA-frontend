7196b936ab80346c4123d65e5bdcadb0f31468d2
Mary Danyliuk <marydanyliuk@gmail.com>
2024-04-15 18:27:34 +0300

    add translate hero

--- src/js/languages.js
+++ src/js/languages.js
@@ -6,8 +6,22 @@ const translations = {
     Мова: 'Language',
     'Будуємо мости Довіри': 'Building bridges of trust',
     'З дня в день': 'From day to day',
-    'Експорт швидко розвиваєш -': 'You develop exports quickly - ',
+    'Експорт швидко розвиваєш -': 'You develop exports quickly -',
     'популярність в світі маєш': 'you are popular in the world',
+    'Експорт – дисципліна для': 'Exporting is a discipline for',
+    'цілеспрямованих і обов’язкових': 'the purposeful and obligatory',
+    'Експорт – дисципліна для цілеспрямованих':
+      'Exporting is a discipline for the purposeful',
+    'і обов’язкових': 'and obligatory',
+    'Експорт – маленькі кроки, які': 'Export - small steps',
+    'з допомогою накопичувального': 'that will lead to the goal',
+    'ефекту приведуть до цілі': 'with the help of a cumulative effect',
+    'Експорт – маленькі кроки, які з допомогою':
+      'Export - small steps that will lead to the goal',
+
+    'накопичувального ефекту приведуть до цілі':
+      'with the help of a cumulative effect',
+
     Швидкiсть: 'Speed',
     Якість: 'Quality',
     Надійність: 'Reliability',
@@ -19,8 +33,15 @@ const translations = {
     Language: 'Мова',
     'Building bridges of trust': 'Будуємо мости Довіри',
     'From day to day': 'З дня в день',
-    'You develop exports quickly - ': 'Експорт швидко розвиваєш -',
+    'You develop exports quickly -': 'Експорт швидко розвиваєш -',
     'you are popular in the world': 'популярність в світі маєш',
+    'Exporting is a discipline for': 'Експорт – дисципліна для',
+    'the purposeful and obligatory': 'цілеспрямованих і обов’язкових',
+    'Export - small steps': 'Експорт – маленькі кроки, які',
+    'Export - small steps that will lead to the goal':
+      'Експорт – маленькі кроки, які з допомогою',
+    'that will lead to the goal': 'з допомогою накопичувального',
+    'with the help of a cumulative effect': 'ефекту приведуть до цілі',
     Speed: 'Швидкiсть',
     Quality: 'Якість',
     Reliability: 'Надійність',
