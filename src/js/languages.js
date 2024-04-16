8fb8e3c2331706b7c06cfd7492a5e41ee9cc87d4
Mary Danyliuk <marydanyliuk@gmail.com>
2024-04-16 11:17:33 +0300

    add translation benefits portfolio

--- src/js/languages.js
+++ src/js/languages.js
@@ -22,10 +22,23 @@ const translations = {
     'накопичувального ефекту приведуть до цілі':
       'with the help of a cumulative effect',
     'Замовити послугу': 'Order',
-
+    'Підхід до праці': 'Approach to work',
     Швидкiсть: 'Speed',
     Якість: 'Quality',
     Надійність: 'Reliability',
+    'Ціна FCA в один клік. Обробка запиту 1 доба. Верифікація ЕКА протягом 2 тижнів (реф посилання). Збільшення Об’єму відвантаження до Х10 – за один місяць':
+      'FCA price in one click. Request processing within 1 day. ECA verification within 2 weeks (ref link). Increase in shipment volume up to X10 - in one month',
+    'Якість Made in Ukraine. Продукція найвищої якості, відповідає міжнародним стандартам, а також процессам виробництва. ISO 22000, ISO 9001, ISO … KOSHER, HALAL. З душею і любов’ю':
+      'Quality Made in Ukraine. Products of the highest quality, comply with international standards and production processes. ISO 22000, ISO 9001, ISO ... KOSHER, HALAL. With soul and love',
+
+    'Виробництво на сучасному обладнанні, багаторічні традиції. Технології затвержені профільними інститутами. Робота на Результат з доставкою - веде до збільшення Довіри один до одного і репутації':
+      'Production on modern equipment, long-standing traditions. Technologies are approved by specialized institutes. Working for the Result with delivery leads to an increase in trust in each other and reputation',
+    'Борошно з зерен пшениці використовується віддавна практично скрізь. Воно незамінне в абсолютно різних сферах, починаючи з кухні і закінчуючи сільським господарством, де служить сировиною для виробництва кормів для тварин. Процеси виготовлення хлібобулочних виробів, приготування тіста, макаронів, пельменів, пирогів, оладок, млинців і різноманітних соусів теж не обходяться без борошна.':
+      'Wheat flour: Flour made from wheat grains has been used for a long time almost everywhere. It is indispensable in a wide variety of areas, from the kitchen to agriculture, where it is used as a raw material for animal feed. The processes of making bakery products, preparing dough, pasta, dumplings, pies, fritters, pancakes and various sauces also require flour',
+    'Борошно пшеничне': 'Wheat flour',
+    'Цукор з буряків': 'Beetroot sugar',
+    'Олія соняшникова': 'Sunflower оil',
+    'Зерно кукурудзи': 'Corn',
   },
   ua: {
     Products: 'Продукція',
@@ -49,9 +62,20 @@ const translations = {
     'with the help of a cumulative effect':
       'накопичувального ефекту приведуть до цілі',
     Order: 'Замовити послугу',
+    'Approach to work': 'Підхід до праці',
     Speed: 'Швидкiсть',
     Quality: 'Якість',
     Reliability: 'Надійність',
+    'FCA price in one click. Request processing within 1 day. ECA verification within 2 weeks (ref link). Increase in shipment volume up to X10 - in one month':
+      'Ціна FCA в один клік. Обробка запиту 1 доба. Верифікація ЕКА протягом 2 тижнів (реф посилання). Збільшення Об’єму відвантаження до Х10 – за один місяць',
+    'Quality Made in Ukraine. Products of the highest quality, comply with international standards and production processes. ISO 22000, ISO 9001, ISO ... KOSHER, HALAL. With soul and love':
+      'Якість Made in Ukraine. Продукція найвищої якості, відповідає міжнародним стандартам, а також процессам виробництва. ISO 22000, ISO 9001, ISO … KOSHER, HALAL. З душею і любов’ю',
+    'Production on modern equipment, long-standing traditions. Technologies are approved by specialized institutes. Working for the Result with delivery leads to an increase in trust in each other and reputation':
+      'Виробництво на сучасному обладнанні, багаторічні традиції. Технології затвержені профільними інститутами. Робота на Результат з доставкою - веде до збільшення Довіри один до одного і репутації',
+    'Wheat flour': 'Борошно пшеничне',
+    'Beetroot sugar': 'Цукор з буряків',
+    'Sunflower oil': 'Олія соняшникова',
+    Corn: 'Зерно кукурудзи',
   },
 };
 
