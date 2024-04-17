4dfa643c232fa457a1ca26cc5c04efec68e7d2ee
Mary Danyliuk <marydanyliuk@gmail.com>
2024-04-17 17:49:41 +0300

    add navigation from burger menu

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -7,3 +7,22 @@ function toggleBurgerMenu(event) {
   event.preventDefault();
   refs.mobileMenu.classList.toggle(`open`);
 }
+
+document.querySelectorAll('.menu__link').forEach(item => {
+  item.addEventListener('click', function () {
+    // Закриття випадаючого меню
+    refs.mobileMenu.classList.remove('open');
+
+    console.log('123');
+
+    // Здійснення плавного переходу до розділу лендінгу
+    const targetSectionId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetSectionId);
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  });
+});
